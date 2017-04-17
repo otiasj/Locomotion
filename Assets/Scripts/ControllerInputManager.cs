d87030a48bdbb5292c36bb734c97a4dd21e5f279
Julien Saito <jsaito@honest.com>
2017-04-17 01:32:14 -0700

    Adding the files

---
+++ Assets/Scripts/ControllerInputManager.cs
@@ -0,0 +1,143 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//This is the implementation, of the InputController interface, responding to user inputs.
+public class ControllerInputManager : MonoBehaviour, InputController {
+
+    //navigation pointer
+	public LineRenderer laser;
+	public GameObject teleporter;
+    public LayerMask laserMask;
+
+    //The Player
+    public GameObject player;
+    public Transform playerCam;
+
+    public float yNudgeAmount = 0.5f; //move the cursor just up enough to not collide with the ground
+	public int laserRange = 15;
+	public bool useInstantTeleportation = false;
+
+	//movement speeds
+	public float dashSpeed = 0.1f;
+    public float movementSpeed = 4f;
+    
+    //teleportation or dashing
+    private bool isDashing;
+	private float lerpTime;
+    private Vector3 targetLocation;
+    private Vector3 dashStartPosition;
+
+	//walking
+    private Vector3 movementDirection;
+    private Vector3 movement;
+
+    // Use this for initialization
+    void Start () {
+	}
+
+	// Update is called once per frame
+	void Update () {
+        if (isDashing) {
+			updateDashingPosition ();
+		}
+	}
+    
+	private void updateDashingPosition() {
+		lerpTime += 1 * dashSpeed;
+		player.transform.position = Vector3.Lerp (dashStartPosition, targetLocation, lerpTime);
+		if (lerpTime >= 1) {
+			isDashing = false;
+			lerpTime = 0;
+		}
+	}
+    
+    //InputController Implementation
+
+	public void aimFrom(Transform transformOrigin) {
+		//set the laser origin location
+		laser.SetPosition (0, transformOrigin.position);
+
+		//find the object colliding with our laser
+		RaycastHit hit;
+		if (Physics.Raycast (transformOrigin.position, transformOrigin.forward, out hit, laserRange, laserMask)) {
+			// the collision point
+			targetLocation = hit.point; 
+
+			// the laser pointer end point
+			laser.SetPosition (1, targetLocation); 
+
+			// the teleport position (yNudgeAmount above the hit object)
+			teleporter.transform.position = new Vector3 (targetLocation.x, 
+				targetLocation.y + yNudgeAmount, 
+				targetLocation.z);
+
+		} else { //if we do not hit anything, 
+
+			// find the location on ground
+			targetLocation = new Vector3 (transformOrigin.forward.x * laserRange + transformOrigin.position.x,
+                transformOrigin.position.y * laserRange + transformOrigin.position.y,
+                transformOrigin.forward.z * laserRange + transformOrigin.position.z);
+
+			//find the ground point down at laserRange away, by sending a raycast vertically down
+			RaycastHit groundRay;
+			if (Physics.Raycast (targetLocation, Vector3.down, out groundRay, laserRange, laserMask)) {
+				targetLocation = new Vector3(transformOrigin.forward.x * laserRange + transformOrigin.position.x,
+					groundRay.transform.position.y,
+                    transformOrigin.forward.z * laserRange + transformOrigin.position.z);
+			}
+
+			//laser end point
+			laser.SetPosition (1, transformOrigin.forward * laserRange + transformOrigin.position);
+
+			// the new position
+			teleporter.transform.position = targetLocation + new Vector3 (0, yNudgeAmount, 0);
+		}
+	}
+
+    public void moveForward() {
+        movementDirection = playerCam.transform.forward;
+        movementDirection = new Vector3(movementDirection.x, 0, movementDirection.z);
+        movement = movementDirection * movementSpeed * Time.deltaTime;
+        player.transform.position += movement;
+    }
+
+    public void enablePointer(bool enabled) {
+        laser.enabled = enabled;
+        teleporter.gameObject.SetActive(enabled);
+    }
+
+    public void moveToPointer() {
+        if (useInstantTeleportation)
+        {
+            player.transform.position = targetLocation;
+        }
+        else
+        {
+            dashStartPosition = player.transform.position;
+            isDashing = true;
+        }
+    }
+
+    //Grabbing stuff
+    public void grab() { }
+
+    public void release() { }
+
+    //menu
+    public void enableMenu() {
+        player.transform.position = new Vector3(0, 0, 0);
+    }
+
+    public void disableMenu() { }
+
+    public void menuUp() { }
+
+    public void menuDown() { }
+
+    public void menuLeft() { }
+
+    public void menuRight() { }
+
+    public void menuSelect() { }
+}
---
+++ Assets/Scripts/ViveControllerManager.cs
@@ -0,0 +1,118 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+** Put this Object on the vive left and/or right controller then set the InputController interface implementation.
+*/
+public class ViveControllerManager : MonoBehaviour {
+
+    public SteamVR_TrackedObject viveController;
+    public GameObject inputController;
+
+    private SteamVR_Controller.Device device;
+    private InputController inputControllerImpl;
+
+    // Use this for initialization
+    void Start () {
+        viveController = GetComponent<SteamVR_TrackedObject>();
+        inputControllerImpl = (InputController)inputController.GetComponent("ControllerInputManager");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        device = SteamVR_Controller.Input((int)viveController.index);
+        if (device.GetPress(SteamVR_Controller.ButtonMask.Grip))
+        {
+            inputControllerImpl.moveForward();
+        }
+
+        if (device.GetPress(SteamVR_Controller.ButtonMask.Trigger))
+        {
+            inputControllerImpl.enablePointer(true);
+            inputControllerImpl.aimFrom(gameObject.transform);
+        }
+
+        if (device.GetPressUp(SteamVR_Controller.ButtonMask.Trigger))
+        {
+            inputControllerImpl.enablePointer(false);
+            inputControllerImpl.moveToPointer();
+        }
+
+        if (device.GetPress(SteamVR_Controller.ButtonMask.Touchpad))
+        {
+            inputControllerImpl.enableMenu();
+        }
+
+        if (device.GetPressDown(SteamVR_Controller.ButtonMask.Touchpad))
+        {
+            detectDirection();
+        }
+
+        if (device.GetPressUp(SteamVR_Controller.ButtonMask.Touchpad))
+        {
+            inputControllerImpl.disableMenu();
+        }
+    }
+
+    Vector2 touchpadLoc;
+    private void detectDirection()
+    {
+        //Read the touchpad values
+        touchpadLoc = device.GetAxis();
+        //Debug.Log(touchpadLoc);
+
+        double targetUpXOld = 0.0; //Old target up value.
+        float targetUpX = (float)targetUpXOld;
+
+        double targetUpYOld = 0.8;
+        float targetUpY = (float)targetUpYOld;
+
+        Vector2 targetUp = new Vector2(targetUpX, targetUpY);
+
+        if (Vector2.Distance(touchpadLoc, targetUp) < .15) //If the distance between finger (touchpadLoc) and the up zone (targetUp) is more than 0 do something.
+        {
+            Debug.Log("Up position met.");
+        }
+
+        double targetDownXOld = -0.0; //Old target down value.
+        float targetDownX = (float)targetDownXOld;
+
+        double targetDownYOld = -0.8;
+        float targetDownY = (float)targetDownYOld;
+
+        Vector2 targetDown = new Vector2(targetUpX, targetUpY);
+
+        if (Vector2.Distance(touchpadLoc, targetDown) > .15)
+        {
+            Debug.Log("Down position met.");
+        }
+
+        double targetLeftXOld = -0.9; //Old target left value.
+        float targetLeftX = (float)targetLeftXOld;
+
+        double targetLextYOld = -0.0;
+        float targetLeftY = (float)targetLextYOld;
+
+        Vector2 targetLeft = new Vector2(targetLeftX, targetLeftY);
+
+        if (Vector2.Distance(touchpadLoc, targetLeft) > .15)
+        {
+            Debug.Log("Left position met.");
+        }
+
+        double targetRightXOld = 0.9; //Old target Right value.
+        float targetRightX = (float)targetRightXOld;
+
+        double targetRightYOld = 0.0;
+        float targetRightY = (float)targetRightYOld;
+
+        Vector2 targetRight = new Vector2(targetRightX, targetRightY);
+
+        if (Vector2.Distance(touchpadLoc, targetRight) > .15)
+        {
+            Debug.Log("Right position met.");
+        }
+    }
+
+}
